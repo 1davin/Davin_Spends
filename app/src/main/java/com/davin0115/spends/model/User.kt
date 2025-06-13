f8aa11fa65fc09b9fa178109fba5e50edf04476f
Davin Ahmad Alaudin <davinahmad53@gmail.com>
2025-06-13 16:51:36 +0700

    Menyimpan data akun pengguna

---
+++ app/src/main/java/com/davin0115/spends/model/User.kt
@@ -0,0 +1,7 @@
+package com.davin0115.spends.model
+
+data class User(
+    val name: String = "",
+    val email: String = "",
+    val photoUrl: String = ""
+)
--- app/src/main/java/com/davin0115/spends/screen/HomeScreen.kt
+++ app/src/main/java/com/davin0115/spends/screen/HomeScreen.kt
@@ -43,10 +43,13 @@ import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
 import androidx.credentials.exceptions.GetCredentialException
+import androidx.datastore.dataStore
 import androidx.navigation.NavHostController
 import com.davin0115.spends.BuildConfig
 import com.davin0115.spends.R
+import com.davin0115.spends.model.User
 import com.davin0115.spends.navigation.Screen
+import com.davin0115.spends.network.UserDataStore
 import com.davin0115.spends.ui.theme.MainColor
 import com.davin0115.spends.ui.theme.SecondColor
 import com.davin0115.spends.ui.theme.poppinsFamily
@@ -61,12 +64,20 @@ import kotlinx.coroutines.launch
 @Composable
 fun HomeScreen(navController: NavHostController) {
     val context = LocalContext.current
+    val dataStore = UserDataStore(context)
+    val user by dataStore.userFlow.collectAsState(User())
 
     Scaffold (
         topBar = {
             GradientTopBarHome(
                 title = "Spends",
-                onProfileClick = { CoroutineScope(Dispatchers.IO).launch { signIn(context) } }
+                onProfileClick = {
+                    if (user.email.isEmpty()) {
+                        CoroutineScope(Dispatchers.IO).launch { signIn(context, dataStore) }
+                    } else {
+                        Log.d("SIGN-IN", "User: $user")
+                    }
+                }
             )
         }
     ) { innerPadding ->
@@ -156,7 +167,7 @@ fun GradientTopBarHome(
     }
 }
 
-private suspend fun signIn(context: Context) {
+private suspend fun signIn(context: Context, dataStore: UserDataStore) {
     val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
         .setFilterByAuthorizedAccounts(false)
         .setServerClientId(BuildConfig.API_KEY)
@@ -169,19 +180,24 @@ private suspend fun signIn(context: Context) {
     try {
         val credentialManager = CredentialManager.create(context)
         val result = credentialManager.getCredential(context, request)
-        handleSignIn(result)
+        handleSignIn(result, dataStore)
     } catch (e: GetCredentialException) {
         Log.e("SIGN-IN", "Error: ${e.errorMessage}")
     }
 }
 
-private fun handleSignIn(result: GetCredentialResponse) {
+private suspend fun handleSignIn(
+    result: GetCredentialResponse,
+    dataStore: UserDataStore) {
     val credential = result.credential
     if (credential is CustomCredential &&
         credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
         try {
             val googleId = GoogleIdTokenCredential.createFrom(credential.data)
-            Log.d("SIGN-IN", "User email: ${googleId.id}")
+            val nama = googleId.displayName ?: ""
+            val email = googleId.id
+            val photoUrl = googleId.profilePictureUri.toString()
+            dataStore.saveData(User(nama, email, photoUrl))
         } catch (e: GoogleIdTokenParsingException) {
             Log.e("SIGN-IN", "Error: ${e.message}")
         }
