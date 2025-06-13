480ed553aae09288ba11a1ce36e657b1aae3f9a7
Davin Ahmad Alaudin <davinahmad53@gmail.com>
2025-06-13 14:44:59 +0700

    Memproses data string

---
+++ app/src/main/java/com/davin0115/spends/model/Gallery.kt
@@ -0,0 +1,7 @@
+package com.davin0115.spends.model
+
+data class Gallery(
+    val judul: String,
+    val keterangan: String,
+    val gambar: String
+)
--- app/src/main/java/com/davin0115/spends/network/GalleryApiService.kt
+++ app/src/main/java/com/davin0115/spends/network/GalleryApiService.kt
@@ -1,20 +1,27 @@
 package com.davin0115.spends.network
 
+import com.davin0115.spends.model.Gallery
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://spends-api.bagasaldianata.my.id/api/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
 interface GalleryApiService {
     @GET("spends")
-    suspend fun getGallery(): String
+    suspend fun getGallery(): List<Gallery>
 }
 
 object GalleryApi {
