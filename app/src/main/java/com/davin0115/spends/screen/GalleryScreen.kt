457d67bf2272afcbcc5388d8e1590be6cf7b7966
Davin Ahmad Alaudin <davinahmad53@gmail.com>
2025-06-13 15:37:14 +0700

    Menangani gambar gagal di-load

--- app/src/main/java/com/davin0115/spends/screen/GalleryScreen.kt
+++ app/src/main/java/com/davin0115/spends/screen/GalleryScreen.kt
@@ -90,6 +90,7 @@ fun ListGallery(gallery: Gallery) {
             contentDescription = stringResource(R.string.image, gallery.judul),
             contentScale = ContentScale.Crop,
             placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.baseline_broken_image_24),
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
         Column (
